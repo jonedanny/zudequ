e6f381754ea6b6243562dbcb43cbcf2e2b01a8c9
e <232271711@qq.com>
2019-12-03 16:52:41 +0800

    20149.12.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'ZdqAdminManager';
+	ngOnInit() {
+		// 全局表格点击选中效果
+		$(document).on('click','.ant-table-row',function(){
+			$('.ant-table-row').removeClass('selected');
+			$(this).addClass('selected')
+		});
+	}
 }
